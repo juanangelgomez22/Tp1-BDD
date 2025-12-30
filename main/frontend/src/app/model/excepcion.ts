e218d39f4913a32b4c6742973bbc6f542a0c8958
Juan Angel Gomez <127906327+juanangelgomez22@users.noreply.github.com>
2025-12-29 23:20:49 -0300

    Delete main directory

--- main/frontend/src/app/model/excepcion.ts
+++
@@ -1,6 +0,0 @@
-export class Excepcion {
-
-  constructor(public readonly mensaje: string, public readonly causa?: any) {
-
-  }
-}
--- main/java/ar/com/grupoesfera/repartir/exceptions/GrupoInvalidoException.java
+++
@@ -1,4 +0,0 @@
-package ar.com.grupoesfera.repartir.exceptions;
-
-public class GrupoInvalidoException extends RuntimeException {
-}
