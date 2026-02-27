03706a63439e703366e7cc8ae7575fdf585f6ffc
Juan Angel Gomez <127906327+juanangelgomez22@users.noreply.github.com>
2026-02-26 22:19:14 -0300

    Delete fastAcceptanceTest directory

--- fastAcceptanceTest/java/ar/com/grupoesfera/repartir/steps/FastCucumberSteps.java
+++
@@ -1,9 +0,0 @@
-package ar.com.grupoesfera.repartir.steps;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-public class FastCucumberSteps {
-
-}
